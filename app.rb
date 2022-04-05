681c38cd2675dde7fc644e1060759cbf92df306f
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2022-04-05 11:31:57 -0400

    Fix assigment done

--- app.rb
+++ app.rb
@@ -1,6 +1,3 @@
-it 'outputs the string "Pass this test, please." using the print method' do
-    expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
-  end
 
 
 puts "Hello World!"
